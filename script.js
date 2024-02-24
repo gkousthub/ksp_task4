89fda2149f4aaa6991ed1d7645616d14f01fcd63
gkousthub <160623425+gkousthub@users.noreply.github.com>
2024-02-24 22:30:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+alert("HI WELCOME TO MY PAGE!!")
+console.log("Welcome to this page!")
+
+
+let number1=5;
+let number2=8;
+let number3=10;
+
+console.log(number3);
+
+const number4=5;
+console.log(number4);
+
+let number5= 6;
+let fnumber=8.5;
+let string1="kousthub";
+let char1="K";
+let bool=true;
+
+//datatypes in javascript
+
+console.log(typeof(number5));
+console.log(typeof(fnumber));
+console.log(typeof(string1));
+console.log(typeof(char1));
+console.log(typeof(bool));
